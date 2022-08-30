53c4b9285196fd07e5af85d2055c62ac47e25a5b
Dennisaugus <dennisasg@outlook.com>
2022-08-30 10:42:35 -0300

    edit: edit backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "dennisterraformpos"
-    key = "network-catapmibatfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+ # backend "s3" {
+   # bucket = "dennisterraformpos"
+   # key = "network-catapmibatfstate"
+  #  region = "us-east-1"
+ # }
+#}
