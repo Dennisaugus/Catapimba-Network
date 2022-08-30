10cecfa19495647726baa61c68eaa206653b701c
Dennisaugus <dennisasg@outlook.com>
2022-08-30 10:32:34 -0300

    add:first commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "dennisterraformpos"
-#    key = "terraform-network-capatimba.tfstate"
-#    region = "us-east-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "dennisterraformpos"
+    key = "terraform-network-capatimba.tfstate"
+    region = "us-east-1"
+  }
+}
